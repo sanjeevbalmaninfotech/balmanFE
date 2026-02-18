9a824d5857f458ebc26b247c5db990717d64b7f5
Sanjeev <sanjeevbalmaninfotech@gmail.com>
2026-02-18 19:45:47 +0530

    fIX  -  updated code for google index

--- app/PrivacyPolicy/page.tsx
+++ app/PrivacyPolicy/page.tsx
@@ -1,6 +1,14 @@
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navBar";
 import PrivacyPolicyMainSecition from "../components/PrivacyPolicy/PrivacyPolicyMainSecition";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
 
 
 
