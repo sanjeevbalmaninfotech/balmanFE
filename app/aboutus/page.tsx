06aa16e6868e6dcc308529b577005dc10b9693d4
Sanjeev <sanjeevbalmaninfotech@gmail.com>
2026-03-02 20:59:46 +0530

    FIx-  about us page title

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -5,14 +5,17 @@ import WhoAreWe from '../components/aboutUs/WhoAreWe';
 import WhyBalmanSection from '../components/aboutUs/whyBalmanSection';
 import WhatWeActuallyDo from '../components/aboutUs/SectionHeadingProps';
 import WhoWeAreBuiltFor from '../components/aboutUs/WhoWeAreBuiltFor';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: "About Balman Infotech - Your Growth Partner",
+  description: "Learn about Balman Infotech, an end-to-end branding, and advertising agency combining strategy, creativity and technology to boost business growth.",
+
+};
+
+
 export default function AboutExperienceSection() {
-
-
-  const metadata = {
-    title: "About Balman Infotech - Your Growth Partner",
-    description: "Learn about Balman Infotech, an end-to-end branding, and advertising agency combining strategy, creativity and technology to boost business growth.",
-
-  };
 
   return (
     <>
